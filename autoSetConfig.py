169efe5687f1b4af98b439f1b8dc580f3e288d11
simple live <59636304+cvic131@users.noreply.github.com>
2023-06-15 16:05:23 +0800

    Update autoSetConfig.py

--- autoSetConfig.py
+++ autoSetConfig.py
@@ -31,7 +31,7 @@ def runCommand(connect, command: str, timeout: int):
     :param connect: sshClient创建的链接
     :param command: 需要在ssh链接中执行的命令
     :param timeout: 等待命令执行的时长
-    :returns: execl中的所有数据，以DataFrame数据结构返回
+    :returns: 执行命令
     """
     connect.send(command + "\n")
     sleep(timeout)
